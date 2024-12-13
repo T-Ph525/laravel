a31343489ca9a6be96d76184cd0ab5caaa3000e8
Evanʎx <95352755+T-Ph525@users.noreply.github.com>
2024-12-13 03:33:42 +0000

    first

--- config/filesystems.php
+++ config/filesystems.php
@@ -9,7 +9,7 @@ return [
     |
     | Here you may specify the default filesystem disk that should be used
     | by the framework. The "local" disk, as well as a variety of cloud
-    | based disks are available to your application for file storage.
+    | based disks are available to your application. Just store away!
     |
     */
 
@@ -20,11 +20,11 @@ return [
     | Filesystem Disks
     |--------------------------------------------------------------------------
     |
-    | Below you may configure as many filesystem disks as necessary, and you
-    | may even configure multiple disks for the same driver. Examples for
-    | most supported storage drivers are configured here for reference.
+    | Here you may configure as many filesystem "disks" as you wish, and you
+    | may even configure multiple disks of the same driver. Defaults have
+    | been set up for each driver as an example of the required values.
     |
-    | Supported drivers: "local", "ftp", "sftp", "s3"
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
     */
 
@@ -32,17 +32,39 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
-            'serve' => true,
+            'root' => storage_path('app'),
             'throw' => false,
+            'visibility' => 'public',
+        ],
+
+        'extension' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+
+        'db_backup' => [
+            'driver' => 'local',
+            'root' => storage_path('app/backup'),
+        ],
+
+        'root' => [
+            'driver' => 'local',
+            'root' => base_path('/'),
+        ],
+
+        'audio' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/storage',
+            'url' => env('APP_URL').'/public',
+            'visibility' => 'public',
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path(),
+            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
-            'throw' => false,
         ],
 
         's3' => [
@@ -55,7 +77,61 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
+            'visibility' => 'public',
+            'options' => ['ContentDisposition' => 'attachment'],
         ],
+
+        'wasabi' => [
+            'driver' => 's3',
+            'key' => env('WASABI_ACCESS_KEY_ID'),
+            'secret' => env('WASABI_SECRET_ACCESS_KEY'),
+            'region' => env('WASABI_DEFAULT_REGION'),
+            'bucket' => env('WASABI_BUCKET'),
+            'endpoint' => 'https://s3.' . env('WASABI_DEFAULT_REGION') . '.wasabisys.com',
+            'visibility' => 'public',
+            'options' => ['ContentDisposition' => 'attachment'],
+        ],
+
+        'gcs' => [
+            'driver' => 'gcs',
+            'key_file_path' => env('GOOGLE_APPLICATION_CREDENTIALS', null), 
+            'bucket' => env('GOOGLE_STORAGE_BUCKET', 'your-bucket'),
+            'visibility' => 'public', 
+            'visibility_handler' => null, 
+            'metadata' => ['cacheControl'=> 'public,max-age=86400', 'contentDisposition' => 'attachment'], 
+        ],
+
+        'storj' => [
+            'driver' => 's3',
+            'key' => env('STORJ_ACCESS_KEY_ID'),
+            'secret' => env('STORJ_SECRET_ACCESS_KEY'),
+            'region' => 'us-east-1',
+            'bucket' => env('STORJ_BUCKET'),
+            'endpoint' => 'https://gateway.storjshare.io',
+            'visibility' => 'public',
+            'options' => ['ContentDisposition' => 'attachment', 'CacheControl'=> 'public,max-age=86400',],
+        ],
+
+        'dropbox' => [
+            'driver' => 'dropbox',
+            'key' => env('DROPBOX_APP_KEY'),
+            'secret' => env('DROPBOX_APP_SECRET'),
+            'authorization_token' => env('DROPBOX_ACCESS_TOKEN'),
+        ],
+
+        'r2' => [
+            'driver' => 's3',
+            'key' => env('CLOUDFLARE_R2_ACCESS_KEY_ID'),
+            'secret' => env('CLOUDFLARE_R2_SECRET_ACCESS_KEY'),
+            'region' => 'us-east-1',
+            'bucket' => env('CLOUDFLARE_R2_BUCKET'),
+            'url' => env('CLOUDFLARE_R2_URL'),
+            'visibility' => 'public',
+            'endpoint' => env('CLOUDFLARE_R2_ENDPOINT'),
+            'use_path_style_endpoint' => env('CLOUDFLARE_R2_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+
 
     ],
 
@@ -71,7 +147,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+      
     ],
 
 ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,44 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            PermissionsTableSeeder::class,
+            RolesTableSeeder::class,
+            AdsenseSeeder::class,
+            CategorySeeder::class,
+            PaymentPlatformsSeeder::class,
+            LanguagesSeeder::class,
+            TemplateSeeder::class,
+            VoiceoverLanguagesSeeder::class,
+            VoicesSeeder::class,
+            VendorsSeeder::class,
+            ChatsSeeder::class,
+            FrontendStepSeeder::class,
+            FrontendToolSeeder::class,
+            FrontendFeatureSeeder::class,
+            ChatCategorySeeder::class,
+            ChatPromptSeeder::class,
+            EmailsSeeder::class,
+            IntegrationSeeder::class,
+            APISeeder::class,
+            SettingsSeeder::class,
+            SDSeeder::class,
+            PricesSeeder::class,
+            SectionsSeeder::class,
+            SEOSeeder::class,
+            ExtensionSeeder::class,
+            ImageCreditsSeeder::class,
         ]);
     }
 }
